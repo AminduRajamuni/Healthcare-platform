3e8a6ef4db0781cc20bcc768db94dca2492cca1b
SanjulaMohotti <sanjulakalpani1212@gmail.com>
2026-04-04 19:04:24 +0530

    Add PrescriptionRepository interface

---
+++ patient-service/src/main/java/com/healthcare/patientservice/repository/PrescriptionRepository.java
@@ -0,0 +1,11 @@
+package com.healthcare.patientservice.repository;
+
+import com.healthcare.patientservice.entity.Prescription;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface PrescriptionRepository extends JpaRepository<Prescription, Long> {
+	List<Prescription> findByPatientId(Long patientId);
+}
+
